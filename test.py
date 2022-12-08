296153afc41f69359dcd90d3113c49b394ac7b15
Emad_Simplon <e.a.darwich@gmail.com>
2022-12-08 14:02:12 +0100

    new push Emad

--- test.py
+++ test.py
@@ -5,27 +5,39 @@ import numpy as np
 import random
 import datetime
 from faker_food import FoodProvider
+import uuid
 
 
 fake = Faker("fr_FR")
 fake.add_provider(FoodProvider)
 
 
-#     "Carte",
-#     metadata,
-#     db.Column("pays_resto", db.Integer(), primary_key = True),
-#     db.Column("id_items", db.Integer(), db.ForeignKey("Restaurant.id_resto")),
-#     db.Column("id_menu", db.String(35), nullable=False),
+# #     "Association",
+# #     metadata,
+# #     db.Column("id_association", db.Integer(), primary_key = True),
+# #     db.Column("id_ingredient", db.Integer(), db.ForeignKey("Restaurant.id_resto")),
+# #     db.Column("id_item", db.String(35), nullable=False),
+ 
+
+data = {}
+for i in range(0, int(100)):
+        data[i]={}
+        data[i]['id_association'] = uuid.uuid4()
 
 
 
-df1 = pd.DataFrame()
-resto = pd.read_csv("CSV/adresse.csv")
-menu = pd.read_csv("CSV/menu.csv")
+df1 = pd.DataFrame(data).T
+
+ingredient = pd.read_csv("CSV/ingredients.csv")
 itms = pd.read_csv("CSV/items.csv")
 
-df1['pays_resto'] = resto['pays_resto']
+
+df1['id_ingredient'] = ingredient['id_ingredient']
 df1['id_item'] = itms['id_item']
-df1['id_menu'] = menu['id_menu']
-print(df1.head())
-# df1.to_csv("CSV/carte.csv", index=False)
+
+
+
+# # df1.to_csv("CSV/ingredients.csv", index=False)
+
+
+

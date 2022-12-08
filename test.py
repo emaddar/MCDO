0e305c740c6e7c88d419e7bf91641b1f81c06cbd
Emad_Simplon <e.a.darwich@gmail.com>
2022-12-08 11:48:10 +0100

    new add

--- test.py
+++ test.py
@@ -4,29 +4,29 @@ import pandas as pd
 import numpy as np
 import random
 import datetime
+from faker_food import FoodProvider
+
 
 fake = Faker("fr_FR")
+fake.add_provider(FoodProvider)
 
 
 
 
 
 
-
-# sample_list = ["directeur", "manager", "employee"]
+sample_list = ["Coca", "Fanta", "Sprit", "Oasis"]
+dessert = ["Hot Fudge Sundae", "Baked Apple Pie", "Hot Caramel Sundae", "M&M McFlurry", "Strawberry Shake"]
 data = {}
 for i in range(0, int(1000)):
         data[i]={}
-        data[i]['num_de_transaction'] = i
-        data[i]['mois'] = random.randint(1, 12)
-        data[i]['annee'] = random.randint(1990, 2022)
-        data[i]['nbr_heures_de_tavail'] = random.randint(25, 35)
-        data[i]['montant_historique'] = random.randint(900, 1600)
-        data[i]['note_manager'] = random.randint(6, 10)
+        data[i]['id_item'] = i
+        data[i]['nom_item'] = fake.dish()
+        data[i]['boisson_taille'] = random.randint(0, 1)
+        data[i]['prix_ventre_menu'] = random.randint(7, 14)
+
 
 df1 = pd.DataFrame(data).T
 
-df = pd.read_csv("CSV/personnels.csv")
-df1["id_personnel"] = df['id_personnel']
-# print(df1.head())
-df1.to_csv("CSV/comptabilite.csv", index=False)
+print(df1.head())
+# df1.to_csv("CSV/menu.csv", index=False)
